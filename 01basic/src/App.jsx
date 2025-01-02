bec461b811481dde7c1a45e04f7b45b021f65a23
Santosh Parajuli <santosh.08045@gmail.com>
2025-01-02 15:54:44 +0545

    themeswitcher

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Coffee from "./coffee"
+function App() {
+ const username="santosh"
+
+  return (
+    <>
+    <h1>React sikam hai sathi | {username}</h1>
+    <Coffee/>
+    </>
+  )
+}
+
+export default App
---
+++ 01basic/src/coffee.jsx
@@ -0,0 +1,6 @@
+function Coffee(){
+    return(
+        <h2>One Americano for Santosh</h2>
+    )
+}
+export default Coffee
